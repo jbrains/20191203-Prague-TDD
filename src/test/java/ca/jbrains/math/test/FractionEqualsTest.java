eb5c81d531e0660dade28d57839bc6c38ad151a4
J. B. Rainsberger <me@jbrains.ca>
2019-12-03 20:55:11 +0100

    Replaced specific examples with more-general properties for Fraction.equals().

--- src/test/java/ca/jbrains/math/test/FractionEqualsTest.java
+++ src/test/java/ca/jbrains/math/test/FractionEqualsTest.java
@@ -6,38 +6,54 @@ import net.jqwik.api.arbitraries.IntegerArbitrary;
 import org.junit.jupiter.api.Assertions;
 
 public class FractionEqualsTest {
-    @Data
-    Iterable<Tuple2<Fraction, Fraction>> unequalExamples() {
-        return Table.of(
-                Tuple.of(
-                        new Fraction(1, 2),
-                        new Fraction(1, 3)),
-                Tuple.of(
-                        new Fraction(3, 7),
-                        new Fraction(4, 7)),
-                Tuple.of(
-                        new Fraction(5, 7),
-                        new Fraction(26, 35))
-        );
+    @Provide
+    Arbitrary<Tuple2<Fraction, Fraction>> unequalPairsOfFractions() {
+        return Arbitraries.oneOf(sameDenominatorDifferentNumerators(), otherExamples());
     }
 
-    @Property
-    @FromData("unequalExamples")
-    void shouldNotBeEqual(@ForAll Fraction first, @ForAll Fraction second) {
-        Assertions.assertNotEquals(first, second);
+    @Property(tries = 50000)
+    void shouldNotBeEqual(@ForAll("unequalPairsOfFractions") Tuple2<Fraction, Fraction> equalPairs) {
+        Assertions.assertNotEquals(equalPairs.get1(), equalPairs.get2());
     }
 
     @Provide
     Arbitrary<Tuple2<Fraction, Fraction>> equalPairsOfFractions() {
-        final IntegerArbitrary numerators = Arbitraries.integers();
-        final IntegerArbitrary denominators = Arbitraries.integers().greaterOrEqual(1);
-        final Arbitrary<Integer> multipliers = Arbitraries.integers().filter(n -> n != 0);
+        final IntegerArbitrary numerators = integersNotTooBig();
+        final IntegerArbitrary denominators = integersNotTooBig().greaterOrEqual(1);
+        final Arbitrary<Integer> multipliers = nonZeroIntegersNotTooBig();
         return Combinators.combine(numerators, denominators, multipliers).as(
                 (n, d, m) -> Tuple.of(new Fraction(n, d), new Fraction(n * m, d * m)));
     }
 
-    @Property
+    @Property(tries = 50000)
     void shouldBeEqual(@ForAll("equalPairsOfFractions") Tuple2<Fraction, Fraction> equalPairs) {
         Assertions.assertEquals(equalPairs.get1(), equalPairs.get2());
+    }
+
+    Arbitrary<Tuple2<Fraction, Fraction>> sameDenominatorDifferentNumerators() {
+        return Combinators.combine(integersNotTooBig(), nonZeroIntegersNotTooBig(), nonZeroIntegersNotTooBig()).as(
+                (numerator, denominator, offset) ->
+                        Tuple.of(
+                                new Fraction(numerator, denominator),
+                                new Fraction(numerator + offset, denominator)));
+    }
+
+    Arbitrary<Tuple2<Fraction, Fraction>> otherExamples() {
+        return Combinators.combine(integersNotTooBig(), nonZeroIntegersNotTooBig(), nonZeroIntegersNotTooBig(), nonZeroIntegersNotTooBig()).as(
+                (numerator, denominator, multiplier, offset) ->
+                        Tuple.of(
+                                new Fraction(numerator, denominator),
+                                new Fraction(numerator * multiplier + offset, denominator * multiplier)));
+    }
+
+    // Avoid overflow in a*d == b*c: let a, b, c, d < sqrt(MAX_VALUE)
+    public static final int LIMIT = (int) Math.floor(Math.sqrt(Integer.MAX_VALUE));
+
+    private IntegerArbitrary integersNotTooBig() {
+        return Arbitraries.integers().between(-LIMIT, LIMIT);
+    }
+
+    private Arbitrary<Integer> nonZeroIntegersNotTooBig() {
+        return integersNotTooBig().filter(n -> n != 0);
     }
 }
