60d4d15ca616db147a0a605b3d7e3a87d4462e39
J. B. Rainsberger <me@jbrains.ca>
2019-12-03 20:25:24 +0100

    Added more examples to check Fraction.equals().

--- src/test/java/ca/jbrains/math/test/FractionEqualsTest.java
+++ src/test/java/ca/jbrains/math/test/FractionEqualsTest.java
@@ -1,11 +1,13 @@
 package ca.jbrains.math.test;
 
 import net.jqwik.api.*;
+import net.jqwik.api.Tuple.Tuple2;
+import net.jqwik.api.arbitraries.IntegerArbitrary;
 import org.junit.jupiter.api.Assertions;
 
 public class FractionEqualsTest {
     @Data
-    Iterable<Tuple.Tuple2<Fraction, Fraction>> unequalExamples() {
+    Iterable<Tuple2<Fraction, Fraction>> unequalExamples() {
         return Table.of(
                 Tuple.of(
                         new Fraction(1, 2),
@@ -23,5 +25,19 @@ public class FractionEqualsTest {
     @FromData("unequalExamples")
     void shouldNotBeEqual(@ForAll Fraction first, @ForAll Fraction second) {
         Assertions.assertNotEquals(first, second);
+    }
+
+    @Provide
+    Arbitrary<Tuple2<Fraction, Fraction>> equalPairsOfFractions() {
+        final IntegerArbitrary numerators = Arbitraries.integers();
+        final IntegerArbitrary denominators = Arbitraries.integers().greaterOrEqual(1);
+        final Arbitrary<Integer> multipliers = Arbitraries.integers().filter(n -> n != 0);
+        return Combinators.combine(numerators, denominators, multipliers).as(
+                (n, d, m) -> Tuple.of(new Fraction(n, d), new Fraction(n * m, d * m)));
+    }
+
+    @Property
+    void shouldBeEqual(@ForAll("equalPairsOfFractions") Tuple2<Fraction, Fraction> equalPairs) {
+        Assertions.assertEquals(equalPairs.get1(), equalPairs.get2());
     }
 }
