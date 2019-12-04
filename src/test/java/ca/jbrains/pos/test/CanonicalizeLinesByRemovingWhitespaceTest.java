6fbb8826ee3e6e4cb73d2f6044522b19a35985fc
J. B. Rainsberger <me@jbrains.ca>
2019-12-04 16:45:25 +0100

    Made tests more specific and isolated from each other.

--- src/test/java/ca/jbrains/pos/test/CanonicalizeLinesByRemovingWhitespaceTest.java
+++ src/test/java/ca/jbrains/pos/test/CanonicalizeLinesByRemovingWhitespaceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,8 +19,16 @@ public class CanonicalizeLinesByRemovingWhitespaceTest {
     @Test
     void trimExtraWhitespace() throws Exception {
         checkLinesCanonicalizedAs(
-                Arrays.asList("    ::command 1::   ", "   \t\t   \r   ", "::command 2::     ", "", "", "\t::command 3::\f"),
+                Arrays.asList("    ::command 1::   ", "::command 2::     ", "\t::command 3::\f"),
                 Arrays.asList("::command 1::", "::command 2::", "::command 3::"));
+    }
+
+    @Test
+    void treatWhitespaceOnlyLinesAsEmpty() throws Exception {
+        checkLinesCanonicalizedAs(
+                Arrays.asList("   \t\t   \r   ", "    "),
+                Collections.emptyList());
+
     }
 
     private void checkLinesCanonicalizedAs(List<String> lines, List<String> expectedCanonicalLines) {
