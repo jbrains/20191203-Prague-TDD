6e5db6f796eb9885e9333546281bd57adb30589f
J. B. Rainsberger <me@jbrains.ca>
2019-12-04 15:46:28 +0100

    The SellOneItem Controller can handle selling a single item.

---
+++ src/test/java/ca/jbrains/pos/test/SellOneItemControllerTest.java
@@ -0,0 +1,88 @@
+package ca.jbrains.pos.test;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class SellOneItemControllerTest {
+    @Test
+    void productFound() throws Exception {
+        final Catalog catalog = Mockito.mock(Catalog.class);
+        final Display display = Mockito.mock(Display.class);
+
+        final Price matchingPrice = Price.cents(48725);
+
+        Mockito.when(catalog.findPrice("::barcode::")).thenReturn(matchingPrice);
+
+        new SellOneItemController(catalog, display).onBarcode("::barcode::");
+
+        Mockito.verify(display).displayPrice(matchingPrice);
+    }
+
+    @Test
+    void productNotFound() throws Exception {
+        final Catalog catalog = Mockito.mock(Catalog.class);
+        final Display display = Mockito.mock(Display.class);
+
+        Mockito.when(catalog.findPrice("::missing barcode::")).thenReturn(null);
+
+        new SellOneItemController(catalog, display).onBarcode("::missing barcode::");
+
+        Mockito.verify(display).displayProductNotFoundMessage("::missing barcode::");
+    }
+
+    @Test
+    void emptyBarcode() throws Exception {
+        final Catalog catalog = Mockito.mock(Catalog.class);
+        final Display display = Mockito.mock(Display.class);
+
+        new SellOneItemController(null, display).onBarcode("");
+
+        Mockito.verify(display).displayScannedEmptyBarcodeMessage();
+    }
+
+    public interface Catalog {
+        Price findPrice(String barcode);
+    }
+
+    public interface Display {
+        void displayPrice(Price price);
+
+        void displayProductNotFoundMessage(String missingBarcode);
+
+        void displayScannedEmptyBarcodeMessage();
+    }
+
+    public static class Price {
+        public static Price cents(int centsValue) {
+            return new Price();
+        }
+
+        @Override
+        public String toString() {
+            return "a Price";
+        }
+    }
+
+    public static class SellOneItemController {
+        private final Catalog catalog;
+        private final Display display;
+
+        public SellOneItemController(Catalog catalog, Display display) {
+            this.catalog = catalog;
+            this.display = display;
+        }
+
+        public void onBarcode(String barcode) {
+            if ("".equals(barcode)) {
+                display.displayScannedEmptyBarcodeMessage();
+                return;
+            }
+
+            final Price price = catalog.findPrice(barcode);
+            if (price == null)
+                display.displayProductNotFoundMessage(barcode);
+            else
+                display.displayPrice(price);
+        }
+    }
+}
