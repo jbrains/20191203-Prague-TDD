0b023c3b20548c9b65dfbe7b746a244b0c81971c
J. B. Rainsberger <me@jbrains.ca>
2019-12-05 10:36:22 +0100

    Just to be sure, confirmed that InMemoryCatalog doesn't merely work by accident.

--- src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
+++ src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -2,7 +2,7 @@ package ca.jbrains.pos.test;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import java.util.HashMap;
 import java.util.Collections;
 import java.util.Map;
 
@@ -18,6 +18,18 @@ public class FindPriceInMemoryCatalogTest {
     void productNotFound() throws Exception {
         final InMemoryCatalog catalog = new InMemoryCatalog(Collections.emptyMap());
         Assertions.assertEquals(null, catalog.findPrice("::barcode::"));
+    }
+
+    @Test
+    void productFoundAmongMany() throws Exception {
+        final Price matchingPrice = Price.cents(9214);
+        final InMemoryCatalog catalog = new InMemoryCatalog(new HashMap<>() {{
+            put("something other than ::barcode::", Price.cents(-1));
+            put("::barcode::", matchingPrice);
+            put("::barcode:: is not this thing", Price.cents(-2));
+            put("not ::barcode::", Price.cents(-3));
+        }});
+        Assertions.assertEquals(matchingPrice, catalog.findPrice("::barcode::"));
     }
 
     public static class InMemoryCatalog {
