a65edc639fa45ccb8dbeb427c6c52e315f36967b
J. B. Rainsberger <me@jbrains.ca>
2019-12-05 11:01:54 +0100

    ConsoleDisplay now implements Display.

---
+++ src/test/java/ca/jbrains/pos/test/ConsoleDisplay.java
@@ -0,0 +1,20 @@
+package ca.jbrains.pos.test;
+
+import java.util.Locale;
+
+public class ConsoleDisplay implements Display {
+    @Override
+    public void displayPrice(Price price) {
+        System.out.println(String.format(Locale.forLanguageTag("cs"), "%.2f Kč", price.koruna()));
+    }
+
+    @Override
+    public void displayProductNotFoundMessage(String missingBarcode) {
+        System.out.println(String.format("Product not found: %s", missingBarcode));
+    }
+
+    @Override
+    public void displayScannedEmptyBarcodeMessage() {
+        System.out.println("Scanning error: empty barcode");
+    }
+}
