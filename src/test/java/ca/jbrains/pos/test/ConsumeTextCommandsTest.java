b32d012fb81907d181b7d780ea53f1614b655b69
J. B. Rainsberger <me@jbrains.ca>
2019-12-05 14:40:59 +0100

    Removed obsolete code.

--- src/test/java/ca/jbrains/pos/test/ConsumeTextCommandsTest.java
+++ src/test/java/ca/jbrains/pos/test/ConsumeTextCommandsTest.java
@@ -2,16 +2,13 @@ package ca.jbrains.pos.test;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 public class ConsumeTextCommandsTest {
     private List<String> commandsReceived = new ArrayList<>();
@@ -46,13 +43,6 @@ public class ConsumeTextCommandsTest {
     }
 
     private void consumeText(final StreamLines streamLines, CanonicalizeLines canonicalizeLines, Consumer<String> handleCommand) {
-        dispatchCommands(
-                handleCommand,
-                canonicalizeLines.canonicalizeLines(
-                        streamLines.streamAsLines()));
-    }
-
-    private void dispatchCommands(Consumer<String> handleCommand, Stream<String> canonicalLines) {
-        canonicalLines.forEach(handleCommand::accept);
+        canonicalizeLines.canonicalizeLines(streamLines.streamAsLines()).forEach(handleCommand::accept);
     }
 }
