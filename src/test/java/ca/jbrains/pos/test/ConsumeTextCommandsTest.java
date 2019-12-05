3ee0191b3e5a4b242e61ab40bbb7c2d36d5db7ae
J. B. Rainsberger <me@jbrains.ca>
2019-12-05 14:41:22 +0100

    Removed obsolete tests.

--- src/test/java/ca/jbrains/pos/test/ConsumeTextCommandsTest.java
+++ src/test/java/ca/jbrains/pos/test/ConsumeTextCommandsTest.java
@@ -1,47 +1,8 @@
 package ca.jbrains.pos.test;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumeTextCommandsTest {
-    private List<String> commandsReceived = new ArrayList<>();
-
-    @Test
-    void oneCommand() throws Exception {
-        checkLinesConsumedAsCommands(
-                Arrays.asList("::command::"),
-                Arrays.asList("::command::"));
-    }
-
-    @Test
-    void noCommands() throws Exception {
-        checkLinesConsumedAsCommands(
-                Collections.emptyList(),
-                Collections.emptyList());
-    }
-
-    @Test
-    void severalCommands() throws Exception {
-        checkLinesConsumedAsCommands(
-                Arrays.asList("::command 1::", "::command 2::", "::command 3::"),
-                Arrays.asList("::command 1::", "::command 2::", "::command 3::"));
-    }
-
-    private void checkLinesConsumedAsCommands(List<String> lines, List<String> expectedCommands) {
-        consumeText(
-                new StreamLinesFromReader(new StringReader(StreamLinesFromReaderTest.unlines(lines))), rawLines -> rawLines, command -> this.commandsReceived.add(command)
-        );
-
-        Assertions.assertEquals(expectedCommands, commandsReceived, "Wrong commands received.");
-    }
-
     private void consumeText(final StreamLines streamLines, CanonicalizeLines canonicalizeLines, Consumer<String> handleCommand) {
         canonicalizeLines.canonicalizeLines(streamLines.streamAsLines()).forEach(handleCommand::accept);
     }
