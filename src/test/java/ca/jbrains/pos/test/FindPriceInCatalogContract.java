438e4b7b10320909c0da36bf134aede4e84894cd
J. B. Rainsberger <me@jbrains.ca>
2019-12-05 10:42:52 +0100

    Extracted contract tests for Catalog.

---
+++ src/test/java/ca/jbrains/pos/test/FindPriceInCatalogContract.java
@@ -0,0 +1,23 @@
+package ca.jbrains.pos.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public abstract class FindPriceInCatalogContract {
+    @Test
+    void productFound() throws Exception {
+        final Price matchingPrice = Price.cents(9214);
+        final Catalog catalog = createCatalogWith("::barcode::", matchingPrice);
+        Assertions.assertEquals(matchingPrice, catalog.findPrice("::barcode::"));
+    }
+
+    protected abstract Catalog createCatalogWith(String barcode, Price matchingPrice);
+
+    @Test
+    void productNotFound() throws Exception {
+        final Catalog catalog = createCatalogWithout("::barcode::");
+        Assertions.assertEquals(null, catalog.findPrice("::barcode::"));
+    }
+
+    protected abstract Catalog createCatalogWithout(String barcodeToAvoid);
+}
--- src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
+++ src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -1,19 +1,11 @@
 package ca.jbrains.pos.test;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-public class FindPriceInMemoryCatalogTest {
-    @Test
-    void productFound() throws Exception {
-        final Price matchingPrice = Price.cents(9214);
-        final InMemoryCatalog catalog = createCatalogWith("::barcode::", matchingPrice);
-        Assertions.assertEquals(matchingPrice, catalog.findPrice("::barcode::"));
-    }
-
-    private InMemoryCatalog createCatalogWith(final String barcode, Price matchingPrice) {
+public class FindPriceInMemoryCatalogTest extends FindPriceInCatalogContract {
+    @Override
+    protected Catalog createCatalogWith(final String barcode, Price matchingPrice) {
         return new InMemoryCatalog(new HashMap<>() {{
             put(String.format("something other than %s", barcode), Price.cents(-1));
             put(barcode, matchingPrice);
@@ -22,13 +14,8 @@ public class FindPriceInMemoryCatalogTest {
         }});
     }
 
-    @Test
-    void productNotFound() throws Exception {
-        final InMemoryCatalog catalog = createCatalogWithout("::barcode::");
-        Assertions.assertEquals(null, catalog.findPrice("::barcode::"));
-    }
-
-    private InMemoryCatalog createCatalogWithout(String barcodeToAvoid) {
+    @Override
+    protected Catalog createCatalogWithout(String barcodeToAvoid) {
         return new InMemoryCatalog(new HashMap<>() {{
             put(String.format("something other than %s", barcodeToAvoid), Price.cents(-1));
             put(String.format("%s is not this thing", barcodeToAvoid), Price.cents(-2));
