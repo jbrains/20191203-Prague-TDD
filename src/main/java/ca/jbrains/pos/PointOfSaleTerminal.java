9ec2310da610d9fe24a5b9c48563be595d5c53a1
J. B. Rainsberger <me@jbrains.ca>
2019-12-05 11:02:10 +0100

    Put everything together and it just works.

---
+++ src/main/java/ca/jbrains/pos/PointOfSaleTerminal.java
@@ -0,0 +1,22 @@
+package ca.jbrains.pos;
+
+import java.util.HashMap;
+
+public class PointOfSaleTerminal {
+    public static void main(String[] args) {
+        final SellOneItemController sellOneItemController = new SellOneItemController(
+                new InMemoryCatalog(
+                        new HashMap<>() {{
+                            put("12345", Price.cents(17300));
+                            put("23456", Price.cents(29850));
+                        }}
+                ),
+                new ConsoleDisplay()
+        );
+
+        sellOneItemController.onBarcode("12345");
+        sellOneItemController.onBarcode("23456");
+        sellOneItemController.onBarcode("99999");
+        sellOneItemController.onBarcode("");
+    }
+}
