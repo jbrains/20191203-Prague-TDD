67bd171e4543bcfd5f14e236fe3e207d79ce529b
J. B. Rainsberger <me@jbrains.ca>
2019-12-05 15:00:41 +0100

    We can route any command as long as it's valid.

--- src/test/java/ca/jbrains/pos/test/RoutePointOfSaleCommandsTest.java
+++ src/test/java/ca/jbrains/pos/test/RoutePointOfSaleCommandsTest.java
@@ -6,6 +6,9 @@ import org.mockito.Mockito;
 import java.util.function.Consumer;
 
 public class RoutePointOfSaleCommandsTest {
+    // CONTRACT We can assume that all commands are canonicalized,
+    // which means that they are not empty and are trimmed.
+
     @Test
     void quit() throws Exception {
         final Consumer<String> quitCommand = Mockito.mock(Consumer.class, "quit command");
@@ -16,7 +19,21 @@ public class RoutePointOfSaleCommandsTest {
         Mockito.verify(quitCommand).accept(Mockito.anyString());
     }
 
+    @Test
+    void handleBarcode() throws Exception {
+        final Consumer<String> quitCommand = Mockito.mock(Consumer.class, "quit command");
+        final Consumer<String> handleBarcodeCommand = Mockito.mock(Consumer.class, "handle barcode command");
+
+        routePointOfSaleCommand("::any other command::", quitCommand, handleBarcodeCommand);
+
+        Mockito.verify(handleBarcodeCommand).accept("::any other command::");
+
+    }
+
     private void routePointOfSaleCommand(String command, Consumer<String> quitCommand, Consumer<String> handleBarcodeCommand) {
-        quitCommand.accept(command);
+        if ("q".equals(command))
+            quitCommand.accept(command);
+        else
+            handleBarcodeCommand.accept(command);
     }
 }
