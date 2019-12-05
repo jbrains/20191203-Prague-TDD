a2a5e94b7697c2566ef88764a5ea64fdb8b27749
J. B. Rainsberger <me@jbrains.ca>
2019-12-05 12:31:03 +0100

    We confirm what happens when we stream lines from an empty stream of input text.

---
+++ src/test/java/ca/jbrains/pos/test/StreamLinesFromReaderTest.java
@@ -0,0 +1,23 @@
+package ca.jbrains.pos.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.StringReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamLinesFromReaderTest {
+    @Test
+    void empty() throws Exception {
+        assertLinesStreamedAs(Collections.emptyList(), "");
+    }
+
+    private static void assertLinesStreamedAs(final List<Object> expectedLines, final String text) {
+        final StreamLinesFromReader streamLinesFromReader = new StreamLinesFromReader(new StringReader(text));
+        Assertions.assertEquals(
+                expectedLines,
+                streamLinesFromReader.streamAsLines().collect(Collectors.toList()));
+    }
+}
