ffa5227f4ee7a46ee66574f91cbfcf0f48979d0f
J. B. Rainsberger <me@jbrains.ca>
2019-12-05 12:45:26 +0100

    Added some tricky special case tests for streaming lines from a Reader.

--- src/test/java/ca/jbrains/pos/test/StreamLinesFromReaderTest.java
+++ src/test/java/ca/jbrains/pos/test/StreamLinesFromReaderTest.java
@@ -34,12 +34,35 @@ public class StreamLinesFromReaderTest {
                 unlines(Arrays.asList("::line 1::", "::line 2::", "::line 3::")) + System.lineSeparator());
     }
 
+    @Test
+    void preserveEmptyLines() throws Exception {
+        assertLinesStreamedAs(
+                Arrays.asList("::line 1::", "", "::line 2::", "", "::line 3::"),
+                unlines(Arrays.asList("::line 1::", "", "::line 2::", "", "::line 3::")));
+    }
+
+    @Test
+    void throwsAwayTrailingLineSeparator() throws Exception {
+        // It looks like 6 empty lines, but it's really 5 empty lines plus a trailing line separator
+        assertLinesStreamedAs(
+                nEmptyLines(5),
+                unlines(nEmptyLines(6)));
+
+        assertLinesStreamedAs(
+                nEmptyLines(5),
+                unlines(nEmptyLines(5)) + System.lineSeparator());
+    }
+
+    private List<String> nEmptyLines(final int n) {
+        return Collections.nCopies(n, "");
+    }
+
     // REFACTOR Move to a generic text-processing library
     public static String unlines(List<String> lines) {
         return lines.stream().collect(Collectors.joining(System.lineSeparator()));
     }
 
-    private static void assertLinesStreamedAs(final List<Object> expectedLines, final String text) {
+    private static void assertLinesStreamedAs(final List<String> expectedLines, final String text) {
         final StreamLinesFromReader streamLinesFromReader = new StreamLinesFromReader(new StringReader(text));
         Assertions.assertEquals(
                 expectedLines,
